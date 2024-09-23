9e7fac217bf01bc7df2e01baa759f989404dc855
Filippo Venturi <fventuri@onit.it>
2024-09-23 13:01:28 +0200

    Test su sugar

--- Laboratorio1/Laboratorio1/Laboratorio1.Tests/Fundamentals/7_Sugar.cs
+++ Laboratorio1/Laboratorio1/Laboratorio1.Tests/Fundamentals/7_Sugar.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xunit.Abstractions;
 
 namespace Laboratorio1.Tests.Fundamentals
 {
-    internal class Sugar
+    public class Sugar
     {
+        private readonly ITestOutputHelper OutputHelper;
+
+        public Sugar(ITestOutputHelper outputHelper)
+        {
+            this.OutputHelper = outputHelper;
+        }
+
+        /// <summary>
+        /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/properties#auto-implemented-properties"/>
+        /// </summary>
+        public class Bee
+        {
+            public string Name { get; set; } = string.Empty;
+
+            public int PollenGathered { get; set; }
+
+            public string Motto { get; set; } = string.Empty;
+        }
+
+        /// <summary>
+        /// Crea un nuovo oggetto di tipo Ape e valorizza le proprietà Name e PollenGathered usando la sintassi al link seguente.
+        /// <see href="https://learn.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/object-and-collection-initializers#object-initializers"/>
+        /// </summary>
+        [Fact]
+        public void FastWayToSetupAClass()
+        {
+            Bee bee = null;
+
+            // TO-DO
+
+            Assert.NotNull(bee);
+            Assert.Equal("Bzz", bee.Name);
+            Assert.Equal(5, bee.PollenGathered);
+            Assert.Equal("You gota bee-lieve", bee.Motto);
+        }
     }
 }
